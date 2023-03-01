f977bed58016152e08baa3a08ad49632ab72555d
D7MeKz <hwanseo6417@naver.com>
2023-03-01 21:28:22 +0900

    [+] C#-foreach

---
+++ C#/Practice/foreach/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class ForeachSample
+{
+    static void Main()
+    {
+        int []cells = {1, 2, 3, 4, 5, 6, 7};
+        int add_value=0;
+        System.Console.Write("This is foreach Sample\n");
+        foreach (int cell in cells)
+        {   
+            add_value += cell;
+            if(add_value < 10)
+            {
+                System.Console.Write($"{add_value}\n");
+            }
+        }
+    }
+}
