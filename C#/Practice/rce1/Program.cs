cb74ffbaeb2b10bde3310e9c00c4cf6fff9c645a
D7MeKz <hwanseo6417@naver.com>
2023-03-02 23:38:33 +0900

    [+] C#-RCE

---
+++ C#/Practice/rce1/Program.cs
@@ -0,0 +1,24 @@
+﻿// https://twitter.com/checkymander/status/1630947396002643971?s=20
+using System.Diagnostics;
+
+class Rce1{
+    static void Main()
+    {
+        string executable = "calc.exe";
+        Process process = new Process
+        {
+            StartInfo = new ProcessStartInfo()
+            {
+                UseShellExecute=false,
+                CreateNoWindow= true,
+                WindowStyle = ProcessWindowStyle.Hidden,
+                FileName= executable,
+                Arguments = "",
+                RedirectStandardError = true,
+                RedirectStandardOutput=true
+            }
+        };
+        process.Start();
+    }
+}
+
