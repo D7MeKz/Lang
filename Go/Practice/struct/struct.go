5a95afeb2af34f8194e5bfc1a7d2fbf8e5f356c3
D7MeKz <hwanseo6417@naver.com>
2023-03-06 01:13:59 +0900

    [+] Go-struct

--- Go/Practice/struct/struct.go
+++ Go/Practice/struct/struct.go
@@ -1,12 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"unsafe"
+)
 
 type House struct {
 	Address string
 	Size    int
 	Price   float64
 	Type    string
+}
+
+type User struct {
+	Id  int16
+	Id2 float64
+}
+
+// 크기가 낮은것부터 배치해야 메모리를 절약할 수 있다.
+type Padding struct {
+	A int8
+	B int8
+	C float64
 }
 
 func main() {
@@ -20,4 +35,10 @@ func main() {
 	var house2 House = House{"G", 2, 3.12, "H"}
 	fmt.Println("House address: ", house2.Address)
 
+	user := User{12, 2.54}
+	// 메모리 정렬 때문에 원래 10바이트 -> 16 바이트로 변경되었다.
+	fmt.Println("Bytes: ", unsafe.Sizeof(user))
+
+	padding := Padding{1, 2, 3.0}
+	fmt.Println("Bytes Padding: ", unsafe.Sizeof(padding))
 }
