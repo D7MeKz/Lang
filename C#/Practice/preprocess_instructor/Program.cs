61ad90fa4afb9c8fbf62d89907b2fadebfa6d273
D7MeKz <hwanseo6417@naver.com>
2023-03-08 09:55:45 +0900

    [+] C#-Preprocess Instructor

---
+++ C#/Practice/preprocess_instructor/Program.cs
@@ -0,0 +1,12 @@
+﻿#pragma warning disable CS1030 // 해제
+#pragma warning restore CS1030 // 복구 
+#nullable enable // null 사용 참조 형식 기능을 킨다. 
+using System;
+
+class PreprocessInstructor
+{
+    static void Main()
+    {
+        System.Console.Write("Hello World!");
+    }
+}
