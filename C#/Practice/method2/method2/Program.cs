09d10ce939914108bded7b9e77402bf354f79c06
D7MeKz <hwanseo6417@naver.com>
2023-03-11 12:26:21 +0900

    [+] C#-Method

---
+++ C#/Practice/method2/method2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Method2
+{
+    static void Main()
+    {
+        string firstname = "D7";
+        string lastname = "MeKz!";
+        test1(ref firstname, ref lastname);
+        System.Console.Write($@"{firstname} {lastname}");
+    }
+
+    // 참조 
+    static void test1(ref string firstname, ref string lastname)
+    {
+        firstname = firstname + "Ref!!";
+        lastname = lastname + "ref!!!!!";
+                
+    }
+}
