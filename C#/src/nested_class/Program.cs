3e1b9607f474e5da00c1846c7fc4b844e734f6ff
D7MeKz <hwanseo6417@naver.com>
2023-04-28 13:19:07 +0900

    [C#] nested class

---
+++ C#/src/nested_class/Program.cs
@@ -0,0 +1,48 @@
+﻿using System.Threading;
+
+// 중첩 클래스는 그다지 좋은 방법은 아니다. 
+class Program
+{
+    private class CommandLine
+    {
+        public string? Action {get; }
+        public string? Id{get;}
+        public string? FirstName{get;}
+        public string? LastName{get;}
+
+        
+        public CommandLine(string[] arguments)
+        {
+            for(int argumentCounter=0; argumentCounter <arguments.Length ; argumentCounter++)
+            {
+                switch(argumentCounter)
+                {
+                    case 0:
+                        Action = arguments[0].ToLower();
+                        break;
+                    case 1:
+                        Id=arguments[1];
+                        break;
+                    case 2:
+                        FirstName = arguments[2];
+                        break;
+                    case 3:
+                        LazyThreadSafetyMode = arguments[3];
+                        break;
+                
+                }
+            }
+        }
+    }
+
+    static void Main(string[] args)
+    {
+        CommandLine commandLine = new CommandLine(args);
+        switch(commandLine.Action)
+        {
+            case "new":
+                System.Console.WriteLine("New!!");
+                break;
+        }
+    }
+}
