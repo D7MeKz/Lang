9338fbbf830ef19905ad405b39896dd2f31555dc
D7MeKz <hwanseo6417@naver.com>
2023-04-30 23:53:46 +0900

    [C#] Child_process

---
+++ C#/src/child_process/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+public class PdaItem
+{
+    [DisallowNull]
+    public string Name{get; set;}
+    public DateTime LastUpdated {get; set;}
+
+}
+
+public class Contact : PdaItem
+{
+    public string Address {get; set;}
+    public string Phone{get; set;}
+}
+
+public class Program
+{
+    public static void Main()
+    {
+        Contact contact = new Contact();
+        contact.Name = "Inigo Montoya";
+        System.console.WriteLine(contact.Name);
+    }
+}
