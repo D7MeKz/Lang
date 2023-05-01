fb964e3b5f369674932a2679dcffdf40c41242e6
D7MeKz <hwanseo6417@naver.com>
2023-05-01 10:22:26 +0900

    [Go] list

--- Go/Practice/list/list.go
+++ Go/Practice/list/list.go
@@ -11,8 +11,8 @@ func main() {
 	v := list.New()
 	e4 := v.PushBack(4)
 	e1 := v.PushFront(1)
-	v.InsertBefore(3, e4)
-	v.InsertAfter(2, e1)
+	v.InsertBefore(3, e4) // e4 요소 앞에 삽입
+	v.InsertAfter(2, e1)  // e1 요소 뒤에 삽입
 
 	for e := v.Front(); e != nil; e = e.Next() {
 		fmt.Print(e.Value)
