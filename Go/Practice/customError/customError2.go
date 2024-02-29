26650b76a9443520ae42159f18dff9bc2247fafb
D7MeKz <damekyung98@gmail.com>
2024-02-29 10:27:29 +0900

    [Go] Error Handling

--- Go/Practice/customError/customError2.go
+++ Go/Practice/customError/customError2.go
@@ -2,11 +2,13 @@ package main
 
 import "fmt"
 
+// 에러 구조체 선언
 type PasswordError struct {
 	Len        int
 	RequireLen int
 }
 
+// Error 메서드 -> PasswordError를 error 인터페이스로써 사용 가능
 func (err PasswordError) Error() string {
 	return "암호 길이가 짧습니다."
 }
@@ -21,7 +23,7 @@ func RegisterAccount(name, password string) error {
 func main() {
 	err := RegisterAccount("myId", "myPw")
 	if err != nil {
-		if errorInfo, ok := err.(PasswordError); ok {
+		if errorInfo, ok := err.(PasswordError); ok { // return Interface
 			fmt.Printf("%v Len:%d RequireLen: %d\n", errorInfo, errorInfo.Len, errorInfo.RequireLen)
 		}
 	} else {
